ec74c7c7c2e1f9e164bf80a8dd288b4990084940
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-03-21 11:55:58 -0400

    builds email and password checker using regex

--- src/regex/patterns.py
+++ src/regex/patterns.py
@@ -5,12 +5,21 @@ to learn more
 regex101.com
 to practice
 https://regexone.com/
+
+email validation r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+
 """
 
 import re
 
-pattern = re.compile(r'([a-zA-Z]).([a])')
-string = 'search this inside of this text yo!'
-
-
+pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+string = 'fake_email@email.org'
+pattern2 = re.compile(r"[a-zA-Z0-9$%#@]{8,}")
+password = 'difaj$wEDJO%sjdi'
 a = pattern.search(string)
+print(a)
+check = pattern2.fullmatch(password)
+
+print(check)
+# At least 8 char long
+# contain any sort of letters, numbers, $%#@
